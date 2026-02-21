082e483194661d6940d89c41e77204d309a448d4
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-02-21 13:46:10 +0500

    Commit 2 (необязательные правки + .idea в .gitignore, Спринт 26

--- app/src/main/java/com/example/jetpackcomposecontacts/ui/theme/Contact.kt
+++ app/src/main/java/com/example/jetpackcomposecontacts/ui/theme/Contact.kt
@@ -1,5 +1,8 @@
 package com.example.jetpackcomposecontacts.ui.theme
 
+import androidx.compose.runtime.Immutable
+
+@Immutable // пометил так, чтобы дать понять Compose, что класс НЕизменяемый 100%, и в нем нет var, MutableList<String>
 data class Contact(
     val name: String, //Имя
     val surname: String? = null, //Отчество
@@ -9,4 +12,25 @@ data class Contact(
     val phone: String, //Телефон
     val address: String, //Адрес
     val email: String? = null, //E-mail
-)
+) {
+    // переменная создана для подготовки ФИО сразу в дата классе, и чтобы не создавать объекты в UI
+    val fullName: String
+        get() = buildString {
+            append(familyName)
+            append(" ")
+            append(name)
+            if (!surname.isNullOrBlank()) {
+                append(" ")
+                append(surname)
+            }
+        }
+
+    /* другой вариант создания
+    val fullName: String
+    get() = listOfNotNull(
+        familyName,
+        name,
+        surname?.takeIf { it.isNotBlank() }
+    ).joinToString(" ")  */
+
+}
